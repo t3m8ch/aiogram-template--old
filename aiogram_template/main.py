16502512caeb8d4975dfdb3d58bf40672bc520df
Kudyakov Artem <t3m8ch@yandex.ru>
2021-06-22 21:24:10 +0400

    feat: Add the option to use Webhooks

--- aiogram_template/main.py
+++ aiogram_template/main.py
@@ -4,12 +4,36 @@ import logging
 import dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+
+from aiogram_template.config import LongPollingUpdateMethod, \
+    WebhookUpdateMethod, Config
 
 from aiogram_template.handlers import register_handlers
 from aiogram_template import config
 
 
-async def run_async():
+def on_startup(cfg: Config):
+    async def func(dp: Dispatcher):
+        update_method = type(cfg.telegram.update_method)
+        if update_method == WebhookUpdateMethod:
+            await dp.bot.set_webhook(cfg.telegram.update_method.url)
+
+        logging.warning("START BOT!")
+
+    return func
+
+
+async def on_shutdown(dp: Dispatcher):
+    await dp.bot.delete_webhook()
+
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning("BOT STOPPED!")
+
+
+def run():
     # Get config
     dotenv.load_dotenv()
     cfg = config.load_config()
@@ -19,9 +43,9 @@ async def run_async():
         level=cfg.logging.level,
         format=cfg.logging.format
     )
-    logging.warning("START BOT!")
 
     # Base
+    event_loop = asyncio.get_event_loop()
     storage = MemoryStorage()  # TODO: Redis
     bot = Bot(
         token=cfg.telegram.token,
@@ -33,19 +57,24 @@ async def run_async():
     register_handlers(dp)
 
     # Start bot!
-    try:
-        await dp.start_polling()  # TODO: Webhook
-    finally:
-        await dp.storage.close()
-        await dp.storage.wait_closed()
-        await bot.session.close()
+    if type(cfg.telegram.update_method) == LongPollingUpdateMethod:
+        executor.start_polling(
+            dispatcher=dp,
+            on_startup=on_startup(cfg),
+            on_shutdown=on_shutdown,
+            loop=event_loop
+        )
 
-
-def run():
-    try:
-        asyncio.run(run_async())
-    except (KeyboardInterrupt, SystemExit):
-        logging.warning("BOT STOPPED!")
+    elif type(cfg.telegram.update_method) == WebhookUpdateMethod:
+        executor.start_webhook(
+            dispatcher=dp,
+            on_startup=on_startup(cfg),
+            on_shutdown=on_shutdown,
+            loop=event_loop,
+            webhook_path=cfg.telegram.update_method.webhook_path,
+            host=cfg.telegram.update_method.webapp_host,
+            port=cfg.telegram.update_method.webapp_port,
+        )
 
 
 if __name__ == "__main__":
