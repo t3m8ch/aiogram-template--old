d621c43af60bf1dc0ec209466ba73da5a8ddd1f5
Kudyakov Artem <t3m8ch@yandex.ru>
2021-07-15 16:16:54 +0400

    fix(di): Inject dependencies into handlers
    Make it possible to inject dependencies into handlers

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,10 @@
+import sys
+
 from . import general
+from bot import di
 
 
-def register_handlers(dp):
+def register_handlers(dp, di_container: di.Container):
     """A function that registers all handlers.
     Example of registration of several handlers:
 
@@ -15,3 +18,5 @@ def register_handlers(dp):
     Remember that the order of handlers is important!
     """
     general.router.register_handlers(dp)
+
+    di_container.wire(packages=[sys.modules[__name__]])
